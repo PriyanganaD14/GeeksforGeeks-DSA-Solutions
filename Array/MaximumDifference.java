5cc165ee85859c57e1c3f23601114052d64d2cc5
Priyangana Das <56598929+PriyanganaD14@users.noreply.github.com>
2021-06-19 00:40:29 +0530

    Update MaximumDifference.java

--- Array/MaximumDifference.java
+++ Array/MaximumDifference.java
@@ -29,7 +29,7 @@ public class MaximumDifference {
 
         int ans=arr[1]-arr[0];
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 int diff=arr[j]-arr[i];
                 if(diff>ans){
